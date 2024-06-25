a7509281fdd062fbabe09477b34775c0b98e7e54
Samuel <ampahsamuel@gmail.com>
2024-06-25 01:18:59 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import Header from "./Components/Header";
+import React, { useState, createContext, useEffect } from "react";
+import Home from "./Pages/Home";
+
+export const ThemeContext = createContext();
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    setTheme(
+      localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+    );
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div
+        className={`${theme} ${
+          theme === "dark" ? " bg-[#333333] text-white" : null
+        } min-h-[100vh]`}
+      >
+        <Header />
+        <Home />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
